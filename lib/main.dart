af3d60686aef0a2e13755fb8af0c6de7e014d207
Nitin Jiwnani <jiwnani01@gmail.com>
2022-06-21 10:51:26 +0530

    Containers

--- lib/main.dart
+++ lib/main.dart
@@ -2,163 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
-  debugPaintSizeEnabled = true;
+  debugPaintSizeEnabled = false; // Set to true for visual layout
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Layout Demo',
-      home: buildHomePage('Strawberry Pavlova Recipe'),
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Layout Demo'),
+        ),
+        body: Center(child: _buildImageColumn()),
+      ),
     );
   }
 
-  Widget buildHomePage(String title) {
-    const titleText = Padding(
-      padding: EdgeInsets.all(20),
-      child: Text(
-        'Strawberry Pavlova',
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          letterSpacing: 0.5,
-          fontSize: 30,
-        ),
+  Widget _buildImageColumn() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.black26,
       ),
-    );
-
-    const subTitle = Text(
-      'Pavlova is a mergiuena based desert named after the Russian balleriana'
-      'Anna Pavlova. Pavloca features a crisp crust and soft'
-      'Topped with fruit and whipped cream.',
-      textAlign: TextAlign.center,
-      style: TextStyle(
-        fontFamily: 'Georgia',
-        fontSize: 25,
-      ),
-    );
-
-    var stars = Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        const Icon(Icons.star, color: Colors.black),
-        const Icon(Icons.star, color: Colors.black),
-      ],
-    );
-
-    final ratings = Container(
-      padding: const EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          stars,
-          const Text(
-            '170 Reviews',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w800,
-              fontFamily: 'Roboto',
-              letterSpacing: 0.5,
-              fontSize: 20,
-            ),
-          ),
-        ],
-      ),
-    );
-
-    const descTextStyle = TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.w800,
-      fontFamily: 'Roboto',
-      letterSpacing: 0.5,
-      fontSize: 18,
-      height: 2,
-    );
-
-    final iconList = DefaultTextStyle.merge(
-      style: descTextStyle,
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: [
-                Icon(Icons.kitchen, color: Colors.green[500]),
-                const Text('PREP:'),
-                const Text('25 min'),
-              ],
-            ),
-            Column(
-              children: [
-                Icon(Icons.timer, color: Colors.green[500]),
-                const Text('COOK:'),
-                const Text('1 hr'),
-              ],
-            ),
-            Column(
-              children: [
-                Icon(Icons.restaurant, color: Colors.green[500]),
-                const Text('FEEDS:'),
-                const Text('4-6'),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-
-    final leftColumn = Container(
-      padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
       child: Column(
         children: [
-          titleText,
-          subTitle,
-          ratings,
-          iconList,
-        ],
-      ),
-    );
-
-    final mainImage = Image.asset('assets/images/pavlova.jpg',
-            fit: BoxFit.cover,
-    );
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.fromLTRB(0, 40, 0, 30),
-          height: 600,
-          child: Card(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 440,
-                  child: leftColumn,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [ Expanded(
-                    child: (
-                      mainImage
-                    ),
-                  ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+          _buildImageRow(1),
+          _buildImageRow(3),
+         ],
         ),
-      ),
     );
   }
+
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 10, color: Colors.black38),
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+      ),
+      margin: const EdgeInsets.all(4),
+      child: Image.asset('assets/images/pic1.jpg'),
+    ),
+  );
+
+  Widget _buildImageRow(int imageIndex) => Row(
+    children: [
+      _buildDecoratedImage(imageIndex),
+      _buildDecoratedImage(imageIndex + 1),
+    ],
+  );
 }
+  
